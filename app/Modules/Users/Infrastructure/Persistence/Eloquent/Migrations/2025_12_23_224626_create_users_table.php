1a48cb991bb51a50e9ee76f19de1a2ff15e934f7
Luisander2651 <23307008@utcgg.edu.mx>
2025-12-28 14:22:04 -0600

    Update: User Migration row: Int Id to String Uuid / Feat: Models based on eloquent

--- app/Modules/Users/Infrastructure/Persistence/Eloquent/Migrations/2025_12_23_224626_create_users_table.php
+++ app/Modules/Users/Infrastructure/Persistence/Eloquent/Migrations/2025_12_23_224626_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
         });
         
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
---
+++ app/Modules/Users/Infrastructure/Persistence/Eloquent/Models/UserModel.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Users\Infrastructure\Persistence\Eloquent\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+final class UserModel extends Model
+{
+    protected $table = 'users';
+
+    public $incrementing = false;
+    public $keyType = 'string';
+    public $timestamps = true;
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'password',
+        'status',
+        'role_id',
+    ];
+
+    // Relaciones
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(RoleModel::class, 'role_id');
+    }
+
+
+}
