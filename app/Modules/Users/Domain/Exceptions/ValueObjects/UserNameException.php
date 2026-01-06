394050cd65f21580ce9925f63d873fb83752b367
Luisander2651 <23307008@utcgg.edu.mx>
2026-01-06 14:56:46 -0600

    Feat: Custom Exceptions

---
+++ app/Modules/Users/Domain/Exceptions/ValueObjects/UserNameException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Users\Domain\Exceptions\ValueObjects;
+
+use App\Modules\Users\Domain\ValueObjects\UserName;
+use App\Modules\Users\Domain\Exceptions\ValueObjectsException;
+
+final class UserNameException extends ValueObjectsException
+{
+    public static function invalidLength(UserName $name, int $minLength, int $maxLength): self
+    {
+        return new self("The username length is invalid: {$name->full()}. Expected between {$minLength} and {$maxLength} characters.");
+    }
+}
